ff187012055adc425c3552e88044289593d6ea95
Andrian Aji <andrian@Andrians-MacBook-Air.local>
2023-03-13 15:41:14 +0700

    deploy

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5494;
 const router = require('./routes/index');
-const session = require('express-session')
+const session = require('express-session');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-  secret: 'Secret ',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { 
-    secure: false,
-    sameSite: true
- }
-}))
+app.use(
+  session({
+    secret: 'Secret ',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      sameSite: true,
+    },
+  })
+);
 
 app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
